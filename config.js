58688b09bd1ce7af36a9c76b46c4eff300c8df1e
monicaramosmoreno <monicaramosmoreno@hotmail.com>
2026-07-29 12:53:22 +0200

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.AUTONOMIA_CONFIG = {
-  GOOGLE_CLIENT_ID: "790739697018-qqsl65keujltsrv0u7bo90vr693gr33k.apps.googleusercontent.com",
-  GOOGLE_API_KEY: "AIzaSyBtr_Le_hPbmRSw-YhyyHAN-oqVfwtQuKY"
-};
